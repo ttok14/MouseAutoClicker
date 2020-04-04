14db32963e0c49d76873a97e179ea54f606e3542
LeeYunSeon <ttok14@naver.com>
2020-04-04 19:46:21 +0900

    - first commit

---
+++ MouseClicker/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MouseClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(159, 99);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(91, 35);
+            this.btnRecord.TabIndex = 2;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.OnClickRecordBtn);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Timer (Seconds, 0 means no limit)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "TimeLeft";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 43);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 306);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRecord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+        private Button btnRecord;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
+
---
+++ MouseClicker/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Media;
+
+namespace MouseClicker
+{
+    public enum Mode
+    {
+        Idle,
+        Recording,
+        Activated,
+    }
+
+    public struct SingleAction
+    {
+        public Point pos;
+        public double waitTime;
+    }
+
+    public partial class Form1 : Form
+    {
+        public Mode curMode = Mode.Idle;
+        TimeSpan delay = TimeSpan.FromSeconds(0.1f);
+
+        List<SingleAction> keys = new List<SingleAction>();
+        int curIndex;
+        double remainedTimeForNextClick;
+
+        double recordIntervalTime;
+
+        double remainedTimerSeconds;
+        bool useTimer;
+
+        public Form1()
+        {
+            MouseCallBack.onLeftMouseDown = OnLeftMouseClicked;
+            MouseCallBack.onRightMouseDown = OnRightMouseClicked;
+
+            InitializeComponent();
+            SetMode(Mode.Idle);
+            Loop();
+        }
+
+        private void OnLeftMouseClicked(int x, int y)
+        {
+            if (curMode != Mode.Recording)
+                return;
+
+            keys.Add(new SingleAction() { pos = new Point(x, y), waitTime = recordIntervalTime });
+            recordIntervalTime = 0;
+        }
+
+        private void OnRightMouseClicked(int x, int y)
+        {
+            ProcessNextByContext();
+        }
+
+        void ProcessNextByContext(bool ableExecute = false)
+        {
+            switch (curMode)
+            {
+                case Mode.Idle:
+                    if (ableExecute)
+                        SetMode(Mode.Recording);
+                    break;
+                case Mode.Recording:
+                    if (keys.Count > 0)
+                    {
+                        SetMode(Mode.Activated);
+                    }
+                    break;
+                case Mode.Activated:
+                    SetMode(Mode.Idle);
+                    break;
+            }
+        }
+
+        void SetMode(Mode mode)
+        {
+            var prevMode = curMode;
+            curMode = mode;
+
+            label4.Text = "CurrentMode : " + mode.ToString();
+
+            switch (mode)
+            {
+                case Mode.Idle:
+                    if(prevMode == Mode.Activated)
+                    {
+                        SystemSounds.Hand.Play();
+                        MessageBox.Show("Stop");
+                    }
+                    break;
+                case Mode.Recording:
+                    keys.Clear();
+                    SystemSounds.Hand.Play();
+                    WindowState = FormWindowState.Minimized;
+                    break;
+                case Mode.Activated:
+                    string str = "";
+
+                    for (int i = 0; i < keys.Count; i++)
+                    {
+                        str += keys[i].waitTime + " ";
+                    }
+
+                    curIndex = 0;
+                    remainedTimeForNextClick = keys[0].waitTime;
+
+                    if (double.TryParse(textBox1.Text, out remainedTimerSeconds))
+                    {
+                        useTimer = remainedTimerSeconds > 0;
+                    }
+                    else
+                    {
+                        useTimer = false;
+                    }
+
+                    //                 MessageBox.Show("Start Play : " + keys.Count);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        async void Loop()
+        {
+            while (true)
+            {
+                switch (curMode)
+                {
+                    case Mode.Idle:
+                        break;
+                    case Mode.Recording:
+                        recordIntervalTime += delay.TotalSeconds;
+                        break;
+                    case Mode.Activated:
+                        {
+                            remainedTimeForNextClick -= delay.TotalSeconds;
+                            remainedTimerSeconds -= delay.TotalSeconds;
+
+                            if (remainedTimeForNextClick <= 0)
+                            {
+                                DoAction(keys[curIndex]);
+                                curIndex++;
+
+                                if (curIndex >= keys.Count)
+                                {
+                                    curIndex = 0;
+                                }
+
+                                remainedTimeForNextClick = keys[curIndex].waitTime;
+                            }
+
+                            if (useTimer)
+                            {
+                                if (remainedTimerSeconds <= 0)
+                                {
+                                    SetMode(Mode.Idle);
+                                    remainedTimerSeconds = 0;
+                                }
+                            }
+
+                            label3.Text = useTimer ? "RemainedSeconds : " + remainedTimerSeconds.ToString() : "NoTimer";
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                label1.Text = "CursorPos : " + Cursor.Position.ToString();
+
+                await Task.Delay(delay);
+                this.Text = DateTime.Now.ToString();
+            }
+        }
+
+        private void DoAction(SingleAction singleAction)
+        {
+            var oriPos = Cursor.Position;
+            DoMouseClick(singleAction.pos.X, singleAction.pos.Y);
+
+            Cursor.Position = oriPos;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnClickRecordBtn(object sender, EventArgs e)
+        {
+            ProcessNextByContext(true);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public void DoMouseClick(int x, int y)
+        {
+            Cursor.Position = new Point() { X = x, Y = y };
+            //Call the imported function with the cursor's current position
+            uint X = (uint)x;
+            uint Y = (uint)y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
